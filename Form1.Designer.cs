d1fe189a1a1b6e4ebdfc67beab6c659cbc9e7b5a
Михаил Винер <mvviner@gmail.com>
2016-11-26 23:51:12 +0700

    variuos changes

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -26,6 +26,10 @@
             this.AcceleratedExploration = new System.Windows.Forms.CheckBox();
             this.TotalTurnsToExploreLabel = new System.Windows.Forms.Label();
             this.WorkersScoutingLabel = new System.Windows.Forms.Label();
+            this.TurnLabel = new System.Windows.Forms.Label();
+            this.Event1Label = new System.Windows.Forms.Label();
+            this.Event2Label = new System.Windows.Forms.Label();
+            this.Event3Label = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // AcceleratedExploration
@@ -42,26 +46,66 @@
             // TotalTurnsToExploreLabel
             // 
             this.TotalTurnsToExploreLabel.AutoSize = true;
-            this.TotalTurnsToExploreLabel.Location = new System.Drawing.Point(12, 36);
+            this.TotalTurnsToExploreLabel.Location = new System.Drawing.Point(10, 36);
             this.TotalTurnsToExploreLabel.Name = "TotalTurnsToExploreLabel";
-            this.TotalTurnsToExploreLabel.Size = new System.Drawing.Size(135, 16);
+            this.TotalTurnsToExploreLabel.Size = new System.Drawing.Size(145, 16);
             this.TotalTurnsToExploreLabel.TabIndex = 1;
-            this.TotalTurnsToExploreLabel.Text = "Total turns to explore:";
+            this.TotalTurnsToExploreLabel.Text = "Total turns to explore: 0";
             // 
             // WorkersScoutingLabel
             // 
             this.WorkersScoutingLabel.AutoSize = true;
             this.WorkersScoutingLabel.Location = new System.Drawing.Point(10, 55);
             this.WorkersScoutingLabel.Name = "WorkersScoutingLabel";
-            this.WorkersScoutingLabel.Size = new System.Drawing.Size(115, 16);
+            this.WorkersScoutingLabel.Size = new System.Drawing.Size(125, 16);
             this.WorkersScoutingLabel.TabIndex = 2;
-            this.WorkersScoutingLabel.Text = "Workers scouting:";
+            this.WorkersScoutingLabel.Text = "Workers scouting: 0";
+            // 
+            // TurnLabel
+            // 
+            this.TurnLabel.AutoSize = true;
+            this.TurnLabel.Location = new System.Drawing.Point(10, 80);
+            this.TurnLabel.Name = "TurnLabel";
+            this.TurnLabel.Size = new System.Drawing.Size(48, 16);
+            this.TurnLabel.TabIndex = 3;
+            this.TurnLabel.Text = "Turn: 0";
+            // 
+            // Event1Label
+            // 
+            this.Event1Label.AutoSize = true;
+            this.Event1Label.Location = new System.Drawing.Point(10, 96);
+            this.Event1Label.Name = "Event1Label";
+            this.Event1Label.Size = new System.Drawing.Size(59, 16);
+            this.Event1Label.TabIndex = 4;
+            this.Event1Label.Text = "0 - Event";
+            // 
+            // Event2Label
+            // 
+            this.Event2Label.AutoSize = true;
+            this.Event2Label.Location = new System.Drawing.Point(10, 112);
+            this.Event2Label.Name = "Event2Label";
+            this.Event2Label.Size = new System.Drawing.Size(59, 16);
+            this.Event2Label.TabIndex = 5;
+            this.Event2Label.Text = "1 - Event";
+            // 
+            // Event3Label
+            // 
+            this.Event3Label.AutoSize = true;
+            this.Event3Label.Location = new System.Drawing.Point(10, 128);
+            this.Event3Label.Name = "Event3Label";
+            this.Event3Label.Size = new System.Drawing.Size(59, 16);
+            this.Event3Label.TabIndex = 6;
+            this.Event3Label.Text = "2 - Event";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(379, 322);
+            this.Controls.Add(this.Event3Label);
+            this.Controls.Add(this.Event2Label);
+            this.Controls.Add(this.Event1Label);
+            this.Controls.Add(this.TurnLabel);
             this.Controls.Add(this.WorkersScoutingLabel);
             this.Controls.Add(this.TotalTurnsToExploreLabel);
             this.Controls.Add(this.AcceleratedExploration);
@@ -79,6 +123,10 @@
         private System.Windows.Forms.CheckBox AcceleratedExploration;
         private System.Windows.Forms.Label TotalTurnsToExploreLabel;
         private System.Windows.Forms.Label WorkersScoutingLabel;
+        private System.Windows.Forms.Label TurnLabel;
+        private System.Windows.Forms.Label Event1Label;
+        private System.Windows.Forms.Label Event2Label;
+        private System.Windows.Forms.Label Event3Label;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,7 +7,6 @@ using System.Linq;
 using System.Runtime.Serialization;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
-using System.Drawing.Drawing2D;
 
 namespace EgyptMapHack {
     public partial class Form1 : Form {
@@ -51,6 +50,8 @@ namespace EgyptMapHack {
                 string json = File.ReadAllText(Filename);
                 CurrentSave = JsonConvert.DeserializeObject<EgyptSave>(json);
                 Refresh();
+            } catch (IOException) {
+                StartTimer();
             } catch (Exception ex) {
                 MessageBox.Show(ex.Message + ex.StackTrace);
             }
@@ -62,17 +63,34 @@ namespace EgyptMapHack {
         protected override void OnPaint(PaintEventArgs e) {
             base.OnPaint(e);
             if (CurrentSave == null) return;
-            PointF center = new PointF(ClientSize.Width / 4, ClientSize.Height / 2);
-            float coef = Math.Min(ClientSize.Width / 2, ClientSize.Height / 2) * 0.038f;
+            float sizev = Math.Min(ClientSize.Width - 20, (ClientSize.Height - 180) / 3);
+            float sizeh = Math.Min((ClientSize.Width - 30) / 2, (ClientSize.Height - 20) / 2);
+            bool vertical = sizev > sizeh;
+            float size = Math.Max(Math.Max(sizeh, sizev), 160);
+            RectangleF rect1 = new RectangleF(10, 160, size, size);
+            RectangleF rect2 = new RectangleF(vertical ? 10 : 20 + size, vertical ? rect1.Bottom + 10 : Math.Max(10, 80 - size / 2), size, size * 2);
             int TotalTurnsToExplore = 0;
             int WorkersScouting = 0;
-            DrawMap(e.Graphics, center, coef, 1, CurrentSave.City, ref TotalTurnsToExplore, ref WorkersScouting);
-            DrawMap(e.Graphics, new PointF(center.X * 3, center.Y), coef, 2, CurrentSave.Egypt, ref TotalTurnsToExplore, ref WorkersScouting);
+            DrawMap(e.Graphics, rect1, CurrentSave.City, ref TotalTurnsToExplore, ref WorkersScouting);
+            DrawMap(e.Graphics, rect2, CurrentSave.Egypt, ref TotalTurnsToExplore, ref WorkersScouting);
             TotalTurnsToExploreLabel.Text = "Total turns to explore: " + TotalTurnsToExplore;
             WorkersScoutingLabel.Text = "Workers scouting: " + WorkersScouting;
+            TurnLabel.Text = "Turn: " + CurrentSave.Turn;
+            int cnt = 0;
+            foreach (Event ev in Schedule.EventList.Where(ev => ev.Turn > CurrentSave.Turn)) {
+                Label l = cnt == 0 ? Event1Label : cnt == 1 ? Event2Label : Event3Label;
+                l.Text = ev.Turn.ToString() + " - " + ev.Name;
+                l.ForeColor = ev.Turn == CurrentSave.Turn + 1 ? Color.Red : Color.Black;
+                if (++cnt == 3) break;
+            }
+            if (cnt == 0) Event1Label.Text = "";
+            if (cnt <= 1) Event2Label.Text = "";
+            if (cnt <= 2) Event3Label.Text = "";
         }
-        private void DrawMap(Graphics g, PointF center, float coef, float ratio, Map Map, ref int TotalTurnsToExplore, ref int WorkersScouting) {
-            g.DrawRectangle(Pens.Black, center.X - 13 * coef, center.Y - 13 * coef * ratio, coef * 26, coef * 26 * ratio);
+        private void DrawMap(Graphics g, RectangleF rect, Map Map, ref int TotalTurnsToExplore, ref int WorkersScouting) {
+            PointF center = new PointF(rect.Left + rect.Width / 2, rect.Top + rect.Height / 2);
+            float coef = rect.Width / 26;
+            g.DrawRectangle(Pens.Black, rect.Left, rect.Top, rect.Width, rect.Height);
             foreach (var cell in Map.Cells) {
                 string cultres = cell.CultResourceId;
                 string ressymbol = (from res in CurrentSave.CultResources where res.ID == cultres select res.IncomeResourceId.Substring(0, 1)).FirstOrDefault() ?? "-";
@@ -92,14 +110,14 @@ namespace EgyptMapHack {
                 //    g.DrawEllipse(Pens.Blue, center.X + cell.X * coef - coef, center.Y - cell.Y * coef - coef, coef * 2, coef * 2);
                 //if (cell.CellTypeId == "e_cities")
                 //    g.DrawEllipse(Pens.Brown, center.X + cell.X * coef - coef, center.Y - cell.Y * coef - coef, coef * 2, coef * 2);
-                bool CannotRaidWarning = raiding && TurnsToWait > 0 && cell.WorkersCount > 0 && cell.Tasks[0].Complexity - cell.Tasks[0].Progress == 1;
-                bool ShouldRaidWarning = raiding && TurnsToWait == 0 && cell.WorkersCount == 0;
-                bool ScoutWarning = exploring && TurnsToExplore <= 0 && cell.WorkersCount > 0;
+                bool CannotRaidWarning = raiding && TurnsToWait > 0 && cell.Tasks[0].Complexity - cell.Tasks[0].Progress == 1;
+                bool ShouldRaidWarning = raiding && TurnsToWait <= 0;
+                bool ScoutWarning = exploring && TurnsToExplore <= 0;
                 if (CannotRaidWarning || ScoutWarning)
                     g.DrawEllipse(new Pen(Color.Red, 2), center.X + cell.X * coef - coef, center.Y - cell.Y * coef - coef, coef * 2, coef * 2);
                 if (ShouldRaidWarning)
                     g.DrawEllipse(new Pen(Color.Blue, 2), center.X + cell.X * coef - coef, center.Y - cell.Y * coef - coef, coef * 2, coef * 2);
-                g.DrawString((TurnsToExplore <= 0 ? ressymbol : TurnsToExplore.ToString() + (ressymbol == "-" ? "" : "/" + ressymbol)) + (TurnsToWait <= 0 ? "" : TurnsToWait.ToString()),
+                g.DrawString((TurnsToExplore <= 0 ? ressymbol : TurnsToExplore.ToString() + (ressymbol == "-" ? "" : ressymbol)) + (TurnsToWait <= 0 ? "" : TurnsToWait.ToString()),
                     Font, Brushes.Black, center.X + cell.X * coef - size.Width / 2, center.Y - cell.Y * coef - size.Height / 2);
                 TotalTurnsToExplore += TurnsToExplore;
                 WorkersScouting += exploring ? cell.WorkersCount : 0;
@@ -107,6 +125,97 @@ namespace EgyptMapHack {
         }
         private void AcceleratedExploration_CheckedChanged(object sender, EventArgs e) {
             Refresh();
+        }
+    }
+    public class Event {
+        public int Turn { get; set; }
+        public string Name { get; set; }
+        public Event(int Turn, string Name) { this.Turn = Turn; this.Name = Name; }
+    }
+    public static class Schedule {
+        private static string ScheduleString = @"
+11 - complete the mission ""Survival"" (growth up to 3 people). +5 food
+13 - random event
+16 - tribe event (attack - before turn 25 you should grow up to 5 people)
+22 - random event
+32 - ""The Sanctuary of the Falcon"" trial
+34 - random event (flood)
+35 - pass the trial. +30 food, +40 hammers
+36 - tribe event
+44 - tribe event
+47 - random event
+52 - tribe event
+57 - random event
+61 - ""The Exodus from the Sahara"" trial
+63 - Desertification of the Sahara (-2 food, -2 hammers)
+64 - Refugees (+2 workers)
+64 - pass the trial. +160 culture, +40 prestige
+66 - random event
+68 - the First sand storm (2 districts damaged)
+72 - contact with the Nubians (+ hammers)
+74 - tribe event
+78 - random event
+81 - tribe event
+84 - random event
+86 - incorporation of Eileithyias (+2 food, +2 hammers, +300 culture)
+86 - complete the mission ""The Formation of the nobility"" (train 500 warriors). +100 warriors
+87 - ""The Hierakonpolis chiefdom"" trial. Pass it immediately. +1 food, +2 hammers, +2 authority.
+89 - Zoo
+92 - random event
+96 - tribe event
+99 - random event
+101 - the Caravan from Mesopotamia
+103 - damage of the workshop (1 district)
+105 - tribe event
+108 - random event
+109 - tribe event
+112 - Best days of Hierakonpolis (+ culture)
+115 - random event
+118 - the Trade blockade, the mission to join Thinis begins
+119 - tribe event
+121 - ""The Great drought"" trial. Pass it immediately
+123 - complete the mission to join Thinis. +300 warriors
+124 - random event
+125 - complete the mission to join other tribes
+126 - complete the mission to conquer the tribe of Seth. +10% strength, +20% trade
+127 - tribe event
+132 - Immigration (worker price +20%, effects of events changed)
+133 - random event
+137 - tribe event
+140 - tribe event
+142 - random event
+145 - Necropolis at Abydos (+ authority)
+147 - tribe event
+150 - random event
+151 - ""The Ombos Invasion"" trial. The trial is omitted. +30 strength each turn
+155 - Governors (+5 food or +12 strength or +5 prestige each turn)
+165 - tribe event
+168 - random event
+171 - King Scorpion. Build a tomb not later than the turn 178
+174 - random event
+177 - tribe event
+181 - ""The Civil War"" trial
+183 - pass the trial. +400 authority
+183 - random event
+186 - King Iri-Horus. Build a tomb not later than the turn 192
+189 - tribe event
+192 - King Ka. Build a tomb not later than the turn 199
+194 - random event
+197 - tribe event
+201 - King Narmer. Build a tomb immediately (finish not later than the turn 204).
+202 - random event
+204 - ""The Unification Of Egypt"" trial";
+        public static List<Event> EventList = new List<Event>();
+        static Schedule() {
+            string[] strings = ScheduleString.Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+            foreach (string s in strings) {
+                int pos = s.IndexOf(" - ");
+                if (pos > 0) {
+                    int turn;
+                    if (!int.TryParse(s.Substring(0, pos), out turn)) continue;
+                    EventList.Add(new Event(turn, s.Substring(pos + 3)));
+                }
+            }
         }
     }
     public class Cell {
